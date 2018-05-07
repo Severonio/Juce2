a7b1380c838f5fb26ade8aa8b3b9e85c9d430640
Severonio <36130687+Severonio@users.noreply.github.com>
2018-05-07 19:41:11 +0300

    L7
    Lab7

---
+++ Lab7d/src/pppp/NegativeNumberException.java
@@ -0,0 +1,8 @@
+package pppp;
+
+public class NegativeNumberException extends Exception { // власний клас вийняток
+    public NegativeNumberException(String text){
+        super(text);
+    }
+}
+
